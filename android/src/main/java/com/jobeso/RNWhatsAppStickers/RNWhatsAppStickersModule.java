9fa6b05c2b9a15e319858619873131f34e127704
Esteban Fuentealba <efuentealba@json.cl>
2020-07-17 21:12:16 -0400

    Update RNWhatsAppStickersModule.java

--- android/src/main/java/com/jobeso/RNWhatsAppStickers/RNWhatsAppStickersModule.java
+++ android/src/main/java/com/jobeso/RNWhatsAppStickers/RNWhatsAppStickersModule.java
@@ -5,17 +5,22 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ResolveInfo;
-import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.google.gson.Gson;
+
+
+import org.json.JSONException;
 
 public class RNWhatsAppStickersModule extends ReactContextBaseJavaModule {
   public static final String EXTRA_STICKER_PACK_ID = "sticker_pack_id";
@@ -46,6 +51,44 @@ public class RNWhatsAppStickersModule extends ReactContextBaseJavaModule {
     return context.getPackageName() + ".stickercontentprovider";
   }
 
+
+  @ReactMethod
+  public void createStickerPack(ReadableMap options, Promise promise) throws JSONException {
+
+      String identifier = options.getString("identifier");
+      String name = options.getString("name");
+      String publisher = options.getString("publisher");
+      String trayImageFile = options.getString("tray_image_file");
+      String publisherEmail = options.getString("publisher_email");
+      String publisherWebsite = options.getString("publisher_website");
+      String privacyPolicyWebsite = options.getString("privacy_policy_website");
+      String licenseAgreementWebsite = options.getString("license_agreement_website");
+      ReadableArray readableStickers = options.getArray("stickers");
+
+
+      StickerPack stickerPack = new StickerPack(identifier, name, publisher, trayImageFile, publisherEmail, publisherWebsite, privacyPolicyWebsite, licenseAgreementWebsite);
+      List<Sticker> stickers = new ArrayList<Sticker>();
+      for (int i = 0; i < readableStickers.size(); i++) {
+          ReadableMap stickerMap = readableStickers.getMap(i);
+          List<String> emojis = new ArrayList<>();
+          ReadableArray readableArray = stickerMap.getArray("emojis");
+          for(int j = 0; j < readableArray.size(); j++) {
+              emojis.add(readableArray.getString(j));
+          }
+          stickers.add(new Sticker(stickerMap.getString("image_file"), emojis));
+      }
+      stickerPack.setStickers(stickers);
+      List<StickerPack> stickerPacks = new ArrayList<StickerPack>();
+      stickerPacks.add(stickerPack);
+      Gson gson = new Gson();
+      String JSONOptions = gson.toJson(stickerPacks);
+
+      SharedPreferences sharedPreferences = this.reactContext.getSharedPreferences("WHATSAPP_STICKERS", Context.MODE_PRIVATE);
+      SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+      prefsEditor.putString(StickerContentProvider.STICKERS, JSONOptions.toString() );
+      prefsEditor.commit();
+      promise.resolve(JSONOptions.toString() );
+  }
   @ReactMethod
   public void send(String identifier, String stickerPackName, Promise promise) {
     Intent intent = new Intent();
